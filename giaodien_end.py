3ae8646cfddf646fa66148d65f41d902779fdc30
Minhpham2759 <144518190+Minhpham2759@users.noreply.github.com>
2024-12-04 23:46:32 +0700

    Update giaodien_end.py

--- giaodien_end.py
+++ giaodien_end.py
@@ -1,37 +1,35 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-from tkinter import messagebox
+import subprocess
+import sys
 import ctypes
 
-ctypes.windll.shcore.SetProcessDpiAwareness(1)  # Đảm bảo tỷ lệ DPI chính xác
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
-# Tạo cửa sổ chính với kích thước 1920x1080
+def open_home():
+    root.destroy()
+    subprocess.run([sys.executable, 'giaodien_open.py'])
+
 root = tk.Tk()
 root.title("Game Python")
-
-# Kích thước cửa sổ
 width, height = 1920, 1080
 root.geometry(f"{width}x{height}")
-
 root.resizable(False, False)
 
-# Tải hình nền
-background_image = Image.open("end_background.jpg")
-background_image = background_image.resize((width, height), Image.Resampling.LANCZOS)  # Điều chỉnh kích thước hình ảnh
+background_image = Image.open("source\end_background.jpg")
+background_image = background_image.resize((width, height), Image.Resampling.LANCZOS)
 background_tk = ImageTk.PhotoImage(background_image)
 
-# Tạo Canvas để vẽ
 canvas = tk.Canvas(root, width=width, height=height)
 canvas.pack()
-# Hiển thị hình nền
 canvas.create_image(0, 0, image=background_tk, anchor="nw")
-# Tải hình ảnh nút sound
-play_image2 = Image.open("home_icon.png")
-play_image2 = play_image2.resize((30*5, 30*5), Image.Resampling.LANCZOS)  # Kích thước nhỏ hơn
+
+play_image2 = Image.open("source\home_icon.png")
+play_image2 = play_image2.resize((150, 150), Image.Resampling.LANCZOS)
 play_tk2 = ImageTk.PhotoImage(play_image2)
 
-# Thêm nút sound vào góc màn hình
-play_button2 = tk.Button(root, image=play_tk2, bg="#ffffff", bd=0, highlightthickness=0, relief="flat", activebackground="#ffffff")
-play_button2.place(relx=0.95, rely=0.07, anchor="center")  # Đặt ở góc màn hình
+play_button2 = tk.Button(root, image=play_tk2, bg="#ffffff", bd=0, highlightthickness=0, relief="flat",
+                         activebackground="#ffffff", command=open_home)
+play_button2.place(relx=0.95, rely=0.07, anchor="center")
 
 root.mainloop()
