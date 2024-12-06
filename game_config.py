8a3cb5174541812fee2e18b1245f12c8adc0a264
Minhpham2579 <minh.phamhcmut2759@hcmut.edu.vn>
2024-12-06 21:28:23 +0700

    fix loi game

--- game_config.py
+++ game_config.py
@@ -2,8 +2,8 @@ import os
 
 IMAGE_SIZE = 128
 
-NUM_TILES_SIDE = 6
-NUM_TILES_TOTAL = 36
+NUM_TILES_SIDE = 7
+NUM_TILES_TOTAL = 56
 MARGIN = 8
 
 # Cập nhật kích thước màn hình
@@ -12,4 +12,4 @@ SCREEN_HEIGHT = 1080  # Height của cửa sổ
 
 ASSET_DIR = 'ani'
 ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']
-assert len(ASSET_FILES) == 18
+assert len(ASSET_FILES) == 34
